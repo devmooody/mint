73be67482d418972adb160dc7973060616a7c902
devmooody <developer.mody@gmail.com>
2024-06-11 00:29:54 +0300

    first files

---
+++ mint2/modules/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from "../Header"
+import WorkShowCase from "../WorkShowCase"
+
+const HomePage = () => {
+  return (
+   <><Header /><WorkShowCase /></>
+  )
+}
+
+export default HomePage
